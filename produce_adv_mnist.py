3e9a9888e11b6b17b76f1d07ff03d5f4a21e7291
Ziwei Zhu <wendy@wirelessprv-10-192-162-139.near.illinois.edu>
2018-12-30 21:08:31 -0600

    💖

--- produce_adv_mnist.py
+++ produce_adv_mnist.py
@@ -22,7 +22,7 @@ from skimage import color
 from skimage import io
 from skimage import transform
 
-path = "image_new" #你所希望读的目录, 图片必须目前必须是28 * 28 * 1, rgb 0 ~ 1的黑白图片
+path = "example_image" #你所希望读的目录, 图片必须目前必须是28 * 28 * 1, rgb 0 ~ 1的黑白图片
 NB_EPOCHS = 1
 BATCH_SIZE = 128
 LEARNING_RATE = 0.001
